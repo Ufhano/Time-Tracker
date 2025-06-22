929b79db30e3047baa6fb3de1f01ad05743a49e4
Ufhano <4035709@myuwc.ac.za>
2025-06-22 20:25:37 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,5 +6,19 @@ import './App.css';
 
 function App(){
   const[entries, setEntries] =useState<TimeEntry[]>([]);
-  
+
+  const handleAddEntry =(task:string,hours:number)=>{
+    const newEntry:TimeEntry ={
+      id:crypto.randomUUID(),
+      task,
+      hours,
+    };
+    setEntries([...entries,newEntry]);
+
+  };
+  return(
+    <div className="App">
+      <h1>Mini Time Tracker</h1>
+    </div>
+  )
 }
