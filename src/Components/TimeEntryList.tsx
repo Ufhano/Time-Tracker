aa69e8bb866a16f3b67a89299ae3717e3a78b225
Ufhano <4035709@myuwc.ac.za>
2025-06-22 20:54:49 +0200

    Updates Time Entry List

---
+++ src/Components/TimeEntryList.tsx
@@ -0,0 +1,30 @@
+import type {TimeEntry} from "../types/timeEntry";
+interface TimeEntryListProps{
+    entries:TimeEntry[];
+
+}
+
+export default function TimeEntryList({entries}:TimeEntryListProps){
+    const totalHours =entries.reduce((sum, entry)=> sum+entry.hours,0);
+
+    return(
+        <div>
+            <h2>Time Entries</h2>
+            {entries.length ===0 ?(
+                <p>No time entries yet.</p>
+            ) : (
+                <ul>
+                    {entries.map((entry)=> (
+                        <li key={entry.id}>
+                            <strong>
+                                {entry.task}
+                            </strong>:
+                            {entry.hours}hour(s)
+                        </li>
+                    ) )}
+                </ul>
+            )}
+            <h3>Total Hours:{totalHours}</h3>
+        </div>
+    );
+}
