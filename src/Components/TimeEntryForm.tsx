198ebce2a7fc2a500d64e9659838d3318afdd1ff
Ufhano <4035709@myuwc.ac.za>
2025-06-22 20:00:10 +0200

    Update TimeEntryForm.tsx

--- src/Components/TimeEntryForm.tsx
+++ src/Components/TimeEntryForm.tsx
@@ -12,6 +12,13 @@ export default function TimeEntryForm({ onAdd}: TimeEntryFormProps){
     const handleSubmit =(e:React.FormEvent)=>{
         e.preventDefault();
 
+        const parsedHours=parseFloat(hours);
+
+        if(!task.trim()){
+            alert("this task name can't be empty");
+            return;
+        }
+
         
     }
 }
